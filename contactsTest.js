a74a495f84af384c93aafc4a083712e6c3cf092b
Claudia Diez <claudia_diez@outlook.com>
2022-05-11 13:53:16 +0200

    fallo

--- contactsTest.js
+++ contactsTest.js
@@ -9,7 +9,7 @@ let Maria = new Persona(180,71,1979);
 let contactos = new Contact()
 contactos.objetos.push(Pepe) ;
 contactos.objetos.push(Maria) ;
-
+//Cambio
 console.log(contactos.objetos);
 
 console.log(Pepe.imc());
