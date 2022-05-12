9258b86b8a68cc9fb4a81806313e3cc7fac76ebd
Claudia Diez <claudia_diez@outlook.com>
2022-05-12 14:41:27 +0200

    Ultimos ejercicios2

--- bookTest.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var book_1 = require("./book");
-var libro = new book_1.Book("Titulo de libro", 178, "isbinUno", "Pepita Perez", "unaEditorial");
-libro.toString();
--- contactTest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var person_1 = require("./person");
-var contact_1 = require("./contact");
-var Pepa = new person_1.Person("pepa", 92, "direccion3");
-var PepaDos = new person_1.Person("pepaD", 96, "direccion4");
-var grupo = new contact_1.Contact();
-grupo.people = [Pepa, PepaDos];
-grupo.printCalendar();
